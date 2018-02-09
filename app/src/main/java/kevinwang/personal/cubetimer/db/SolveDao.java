78591aedd7c7fc2f74127c6c9bf272dcb61d97be
Kevin <kevinwang27@berkeley.edu>
2018-02-09 14:39:12 -0800

    added dao

---
+++ app/src/main/java/kevinwang/personal/cubetimer/db/SolveDao.java
@@ -0,0 +1,9 @@
+package kevinwang.personal.cubetimer.db;
+
+/**
+ * Created by kevinwang on 2/9/18.
+ */
+
+public interface SolveDao {
+    
+}
--- app/src/main/java/kevinwang/personal/cubetimer/db/entity/Solve.java
+++ app/src/main/java/kevinwang/personal/cubetimer/db/entity/Solve.java
@@ -1,8 +1,45 @@
 package kevinwang.personal.cubetimer.db.entity;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 /**
  * Created by kevinwang on 2/9/18.
  */
 
+@Entity
 public class Solve {
+    @PrimaryKey
+    private int solveNum;
+
+    @ColumnInfo(name = "solve_time")
+    private String solveTime;
+
+    private String scramble;
+
+
+    public int getSolveNum() {
+        return solveNum;
+    }
+
+    public void setSolveNum(int solveNum) {
+        this.solveNum = solveNum;
+    }
+
+    public String getSolveTime() {
+        return solveTime;
+    }
+
+    public void setSolveTime(String solveTime) {
+        this.solveTime = solveTime;
+    }
+
+    public String getScramble() {
+        return scramble;
+    }
+
+    public void setScramble(String scramble) {
+        this.scramble = scramble;
+    }
 }
